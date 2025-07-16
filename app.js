78d88b618d4f9657fd7018fa6d51eeef3af029e1
Tabuke Ezekiel <tabukeezekiel9@gmail.com>
2025-07-16 16:19:24 +0100

    try embedded form Payment

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ import cors from "cors";
 import router from "./routes/checkout.routes.js";
 
 const app = express();
+app.use(express.static("public"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- routes/checkout.routes.js
+++ routes/checkout.routes.js
@@ -1,40 +1,33 @@
 import express from "express";
 import Stripe from "stripe";
 import dotenv from "dotenv";
-import { STRIPE_SECRET_KEY } from "../config/env.js";
+import { STRIPE_SECRET_KEY, YOUR_DOMAIN } from "../config/env.js";
 dotenv.config();
 
 const router = express.Router();
 const stripe = new Stripe(STRIPE_SECRET_KEY);
-
-// POST /create-checkout-session
 router.post("/create-checkout-session", async (req, res) => {
-  const { amount, propertyId } = req.body;
+  const session = await stripe.checkout.sessions.create({
+    ui_mode: "embedded",
+    line_items: [
+      {
+        // Provide the exact Price ID (for example, price_1234) of the product you want to sell
+        price: "{{PRICE_ID}}",
+        quantity: 1,
+      },
+    ],
+    mode: "payment",
+    return_url: `${YOUR_DOMAIN}/return?session_id={CHECKOUT_SESSION_ID}`,
+  });
 
-  try {
-    const session = await stripe.checkout.sessions.create({
-      payment_method_types: ["card"],
-      line_items: [
-        {
-          price_data: {
-            currency: "usd",
-            product_data: {
-              name: `Property ID: ${propertyId}`,
-            },
-            unit_amount: amount * 100, // amount in cents
-          },
-          quantity: 1,
-        },
-      ],
-      mode: "payment",
-      success_url: `https://betahouse3.vercel.app/success`,
-      cancel_url: `https://betahouse3.vercel.app/cancel`,
-    });
-
-    res.json({ id: session.id });
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
+  res.send({ clientSecret: session.client_secret });
 });
 
-export default router;
+router.get("/session-status", async (req, res) => {
+  const session = await stripe.checkout.sessions.retrieve(req.query.session_id);
+
+  res.send({
+    status: session.status,
+    customer_email: session.customer_details.email,
+  });
+});
