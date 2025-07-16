00517877120aca0c8c454473d6bb7e0233c5ab28
Tabuke Ezekiel <tabukeezekiel9@gmail.com>
2025-07-16 17:05:28 +0100

    Advanced Integration method of stripe

--- routes/checkout.routes.js
+++ routes/checkout.routes.js
@@ -1,33 +1,39 @@
 import express from "express";
 import Stripe from "stripe";
 import dotenv from "dotenv";
-import { STRIPE_SECRET_KEY, YOUR_DOMAIN } from "../config/env.js";
+import { STRIPE_SECRET_KEY } from "../config/env.js";
 dotenv.config();
 
 const router = express.Router();
 const stripe = new Stripe(STRIPE_SECRET_KEY);
-router.post("/create-checkout-session", async (req, res) => {
-  const session = await stripe.checkout.sessions.create({
-    ui_mode: "embedded",
-    line_items: [
-      {
-        // Provide the exact Price ID (for example, price_1234) of the product you want to sell
-        price: "{{PRICE_ID}}",
-        quantity: 1,
-      },
-    ],
-    mode: "payment",
-    return_url: `${YOUR_DOMAIN}/return?session_id={CHECKOUT_SESSION_ID}`,
+
+const calculateOrderAmount = (items) => {
+  // Calculate the order total on the server to prevent
+  // people from directly manipulating the amount on the client
+  let total = 0;
+  items.forEach((item) => {
+    total += item.amount;
   });
+  return total;
+};
 
-  res.send({ clientSecret: session.client_secret });
-});
+router.post("/create-payment-intent", async (req, res) => {
+  const { items } = req.body;
+  console.log(items);
 
-router.get("/session-status", async (req, res) => {
-  const session = await stripe.checkout.sessions.retrieve(req.query.session_id);
+  // Create a PaymentIntent with the order amount and currency
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: calculateOrderAmount(items),
+    currency: "ngn",
+    // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
+    automatic_payment_methods: {
+      enabled: true,
+    },
+  });
 
   res.send({
-    status: session.status,
-    customer_email: session.customer_details.email,
+    clientSecret: paymentIntent.client_secret,
   });
 });
+
+export default router;
